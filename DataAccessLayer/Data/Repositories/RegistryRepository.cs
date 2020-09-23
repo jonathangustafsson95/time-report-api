75c2fbba659de0389ef96ba74f503cfdf422d129
Tamara S. A. Malik <tamara.m2012@gmail.com>
2020-09-23 16:17:46 +0200

    new repository

---
+++ DataAccessLayer/Data/Repositories/RegistryRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommonLibrary.Model;
+using DataAccessLayer.Data.IRepositories;
+using DataAccessLayer.Repositories;
+
+namespace DataAccessLayer.Data.Repositories
+{
+    class RegistryRepository : GenericRepository<Registry>, IRegistryRepository
+    {
+
+        public RegistryRepository(BulbasaurContext context)
+            : base(context)
+        {
+
+        }
+
+        public List<Registry> GetRegistriesLastWeek()
+        {
+            var registries = GetAll();
+            var enumerable = registries.ToList();
+            enumerable.OrderBy(d => d.date);
+            return (List<Registry>)enumerable.Take(7);
+
+
+
+        }
+    }
+    
+    }
