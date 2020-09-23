64fcf4bc3d7cb27e81fd675f6f791a2577bb84d1
challe <im.challe@gmail.com>
2020-09-23 16:10:28 +0200

    reportingcontroller

--- time report api/Controllers/ReportingController.cs
+++ time report api/Controllers/ReportingController.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace time_report_api.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize]
     [ApiController]
     public class ReportingController : ControllerBase
     {
+        [HttpGet]
+        [Route("Bajs")]
+        public ActionResult Bajs()
+        {
+            return StatusCode(500, "Something went wrong!");
+        }
     }
 }
