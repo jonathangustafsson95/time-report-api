e431676e50ba40309905c4884aecf47ea1de9ec7
Tamara S. A. Malik <tamara.m2012@gmail.com>
2020-09-28 15:06:46 +0200

    testingcrap

--- Database UnitTest/GenericRepositoriesTest.cs
+++ Database UnitTest/GenericRepositoriesTest.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.EntityFrameworkCore;
 using DataAccessLayer.Data.Repositories;
@@ -16,26 +17,66 @@ namespace Database_UnitTest
     [TestClass]
     public class GenericRepositoriesTest
     {
-        
+        [TestMethod]
+        public void AddTestInMemory()
+        {
+            var options = new DbContextOptionsBuilder<MyContext>()
+                .UseInMemoryDatabase(databaseName: "Test")
+                .Options;
+
+            using (var context = new MyContext(options))
+            {
+                var UserBla = new User()
+                {
+                    userId = 1,
+                    eMail = "bkabkabadbvd"
+                };
+
+                context.users.Add(UserBla);
+                context.SaveChanges();
+            //var repository = new GenericRepository<User>(context);
+            //repository.Insert(UserBla);
+
+            }
+            //act
+
+        }
+
+
+
         [TestMethod]
         public void TestAddToDataBase()
         {
 
+            var testObject = new User() {userId = 1,eMail = "Bla@bla"};
+            var context = new Mock<BulbasaurDevContext>();
+            var dbSetMock = new Mock<DbSet<User>>();
+            context.Setup(x => x.Set<User>()).Returns(dbSetMock.Object);
+            dbSetMock.Setup(x => x.Add(It.IsAny<User>()).Entity).Returns(testObject);
+
+            //act
+            var repository = new GenericRepository<User>(context.Object);
+            repository.Insert(testObject);
+            //assert
+            context.Verify(x => x.Set<User>());
+            dbSetMock.Verify(x => x.Add(It.Is<User>(y => y == testObject)));
+
+
 
 
             //arrange
-            var testObject = new MissionMember();
-            var context = new Mock<BulbasaurDevContext>();
-            var dbSetMock = new Mock<DbSet<MissionMember>>();
-            context.Setup(x => x.Set<MissionMember>()).Returns(dbSetMock.Object);
-            dbSetMock.Setup(x => x.Add(It.IsAny<MissionMember>()).Entity).Returns(testObject);
+            //var testObject = new MissionMember {missionId = 1, userId = 1,Mission = };
+            //var context = new Mock<BulbasaurDevContext>();
+            //var dbSetMock = new Mock<DbSet<MissionMember>>();
+            //context.Setup(x => x.Set<MissionMember>()).Returns(dbSetMock.Object);
+            //dbSetMock.Setup(x => x.Add(It.IsAny<MissionMember>()).Entity).Returns(testObject);
 
-            //act
-            var repository = new GenericRepository<MissionMember>(context.Object);
-            repository.Insert(testObject);
-            //assert
-            context.Verify(x => x.Set<MissionMember>());
-            dbSetMock.Verify(x => x.Add(It.Is<MissionMember>(y => y == testObject)));
+            ////act
+            //var repository = new GenericRepository<MissionMember>(context.Object);
+            //repository.Insert(testObject);
+            ////assert
+            //context.Verify(x => x.Set<MissionMember>());
+            //dbSetMock.Verify(x => x.Add(It.Is<MissionMember>(y => y == testObject)));
 
 
 
