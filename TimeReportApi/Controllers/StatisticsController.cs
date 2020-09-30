a28d2230b46a9730704978c30c7bd992673de46f
Administrator <>
2020-09-30 15:23:13 +0000

    Updated StatisticsController.cs

--- TimeReportApi/Controllers/StatisticsController.cs
+++ TimeReportApi/Controllers/StatisticsController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace time_report_api.Controllers
+namespace TimeReportApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
