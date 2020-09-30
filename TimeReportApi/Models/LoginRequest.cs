e84d8c88fe310245202522eb750fde72af04c301
Administrator <>
2020-09-30 15:26:32 +0000

    Updated LoginRequest.cs

--- TimeReportApi/Models/LoginRequest.cs
+++ TimeReportApi/Models/LoginRequest.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace time_report_api.Models
+namespace TimeReportApi.Models
 {
     public class LoginRequest
     {
