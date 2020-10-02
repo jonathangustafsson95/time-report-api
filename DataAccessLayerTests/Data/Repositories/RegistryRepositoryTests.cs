5295cd1b782995ceb3fa447d0208ee31e05bedd5
Administrator <>
2020-10-02 06:37:43 +0000

    Updated RegistryRepositoryTests.cs

--- DataAccessLayerTests/Data/Repositories/RegistryRepositoryTests.cs
+++ DataAccessLayerTests/Data/Repositories/RegistryRepositoryTests.cs
@@ -1,18 +1,18 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using DataAccessLayer.Data.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using DataAccessLayer.Data.Repositories;
+// using System;
+// using System.Collections.Generic;
+// using System.Text;
 
-namespace DataAccessLayer.Data.Repositories.Tests
-{
-    [TestClass()]
-    public class RegistryRepositoryTests
-    {
-        [TestMethod()]
-        public void GetRegistriesLastWeekTest()
-        {
-            Assert.Fail();
-        }
-    }
-}
+// namespace DataAccessLayer.Data.Repositories.Tests
+// {
+//     [TestClass()]
+//     public class RegistryRepositoryTests
+//     {
+//         [TestMethod()]
+//         public void GetRegistriesLastWeekTest()
+//         {
+//             Assert.Fail();
+//         }
+//     }
+// }
