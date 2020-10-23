22d60a7e547b007a8f5ea9dff1518bc8f8484fb5
denny <denny@DESKTOP-97SU957>
2020-10-23 13:59:18 +0200

    Statistic

--- DatabaseUnitTest/Controllers/StatisticControllerTest.cs
+++ DatabaseUnitTest/Controllers/StatisticControllerTest.cs
@@ -16,48 +16,67 @@ using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using System.Linq;
 using TimeReportApi.Models.ViewModel;
+using time_report_api.Controllers;
 
-//namespace Database_UnitTest.Controllers
-//{
-//    public class StatisticControllerTest
-//    {
-//        private IHttpContextAccessor httpContextAccessorMock;
-//        public StatisticControllerTest()
-//        {
-//            int userId = 1;
-//            var userIdClaim = A.Fake<Claim>(x => x.WithArgumentsForConstructor(() => new Claim("userId", userId.ToString())));
-//            this.httpContextAccessorMock = A.Fake<IHttpContextAccessor>();
-//            httpContextAccessorMock.HttpContext = A.Fake<HttpContext>();
-//            httpContextAccessorMock.HttpContext.User = A.Fake<ClaimsPrincipal>();
-//            A.CallTo(() => httpContextAccessorMock.HttpContext.User.Claims).Returns(new List<Claim> { userIdClaim });
+namespace Database_UnitTest.Controllers
+{
+    public class StatisticControllerTest
+    {
+        private IHttpContextAccessor httpContextAccessorMock;
+        public StatisticControllerTest()
+        {
+            int userId = 1;
+            var userIdClaim = A.Fake<Claim>(x => x.WithArgumentsForConstructor(() => new Claim("userId", userId.ToString())));
+            this.httpContextAccessorMock = A.Fake<IHttpContextAccessor>();
+            httpContextAccessorMock.HttpContext = A.Fake<HttpContext>();
+            httpContextAccessorMock.HttpContext.User = A.Fake<ClaimsPrincipal>();
+            A.CallTo(() => httpContextAccessorMock.HttpContext.User.Claims).Returns(new List<Claim> { userIdClaim });
+        }
+        [Theory]
+        [MemberData(nameof(GetInternalCustomerData), parameters: 1)]
+        public void GetStatsInternVsCustomer(DateTime startDate,object expected)
+        {
+            List<Registry> dbRegistries = new List<Registry>();
+            Mock<IRegistryRepository> registryRepoMock = new Mock<IRegistryRepository>();
+            registryRepoMock.Setup(r => r.GetRegistriesByDate(It.IsAny<DateTime>(), It.IsAny<DateTime>(), It.IsAny<int>())).Returns(dbRegistries);
 
-//        }
-//        [Theory]
-//        [MemberData(nameof(GetData), parameters: 2)]
-//        public void GetStatsInternVsCustomer(DateTime startDate)
-//        {
+            Mock<IUnitOfWork> mockUOF = new Mock<IUnitOfWork>();
+            mockUOF.Setup(uow => uow.RegistryRepository).Returns(registryRepoMock.Object);
+           
+            var controller = new StatisticsController(mockUOF.Object, httpContextAccessorMock);
 
-//        }
-//        [Theory]
-//        [MemberData(nameof(GetData), parameters: 2)]
-//        public void GetStatsCustomerVsCustomer(DateTime date)
-//        {
+            //Act
+            var result = controller.GetStatsInternVsCustomer(startDate);
 
-//        }
-//        [Theory]
-//        [MemberData(nameof(GetData), parameters: 2)]
-//        public void GetTaskStats(int missionId)
-//        {
-//        }
-//        public static IEnumerable<object[]> GetData(int numTests)
-//        {
-//            var allData = new List<object[]>
-//            {
-//                new object[] { 1,true ,(int)HttpStatusCode.OK },
-//                new object[] { 99,false, (int)HttpStatusCode.InternalServerError },
-//            };
+            if (expected.GetType() != StatusCodes.Status500InternalServerError.GetType())
+            {
+                Assert.IsType(expected.GetType(), result.Value);
+            }
+            else
+            {
+                Assert.Equal(expected, (result.Result as ObjectResult).StatusCode);
+            }
+        }
+        //[Theory]
+        //[MemberData(nameof(GetData), parameters: 2)]
+        //public void GetStatsCustomerVsCustomer(DateTime date)
+        //{
 
-//            return allData.Take(numTests);
-//        }
-//    }
-//}
+        //}
+        //[Theory]
+        //[MemberData(nameof(GetData), parameters: 2)]
+        //public void GetTaskStats(int missionId)
+        //{
+        //}
+        public static IEnumerable<object[]> GetInternalCustomerData(int numTests)
+        {
+            var allData = new List<object[]>
+            {
+                new object[] { new DateTime(2020,10,23) ,new List<StatisticCustomerInternalViewModel>() },
+                new object[] { new DateTime() , (int)HttpStatusCode.InternalServerError },
+            };
+
+            return allData.Take(numTests);
+        }
+    }
+}
